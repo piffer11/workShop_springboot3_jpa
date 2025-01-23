b4f1a14abe751e24af2f9ba0b248966faaa4aba4
Gustavo Piffer <piffersales2017@gmail.com>
2025-01-23 12:59:22 -0300

    project created

---
+++ src/main/java/com/gustavoPiffer/corse/CorseApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoPiffer.corse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoPiffer/corse/CorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoPiffer.corse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
