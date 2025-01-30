9efa81f1d3ef5bbb42f61be7e4d69cfc5fda6566
Gustavo Piffer <piffersales2017@gmail.com>
2025-01-30 09:05:22 -0300

    Exception handling_delete

--- src/main/java/com/gustavoPiffer/corse/services/UserService.java
+++ src/main/java/com/gustavoPiffer/corse/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gustavoPiffer.corse.entities.User;
 import com.gustavoPiffer.corse.repositories.UserRepository;
+import com.gustavoPiffer.corse.services.exceptions.DatabaseException;
 import com.gustavoPiffer.corse.services.exceptions.ResourceNotFoundException;
 
 
@@ -29,7 +32,15 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void delete (Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e ) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	public User uptade (Long id, User obj) {
 		User entity = repository.getReferenceById(id);
